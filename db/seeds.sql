508842ce5715de5128b6934c280aeef6b3917130
Joanna Sowa <joanna.sowa19@gmail.com>
2020-02-16 17:41:07 -0600

    burger app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDouble', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', true);
